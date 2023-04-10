095e0897c378025cb19868b2951b078bae80c735
Joe Bryan <bryan.joe@gmail.com>
2023-04-10 14:58:32 -0400

    move react-scripts to dev-deps

---
+++ build/precache-manifest.b80ffd71d1f407ad097d7856b47809ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "515e4dbc0f714c3d1d47",
+    "url": "/static/css/main.eb1c5791.chunk.css"
+  },
+  {
+    "revision": "515e4dbc0f714c3d1d47",
+    "url": "/static/js/main.515e4dbc.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6e8bcf7365394e3c5d138e9db89f9b90",
+    "url": "/index.html"
+  }
+];
